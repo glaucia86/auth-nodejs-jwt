6ea9d28aaad2f49163ca565c0018b46eb9efdb7f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-04-13 22:22:36 -0300

    Alteração no arquivo 'config.js'

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+
+Arquivo: config.js
+Data: 12/04/2017
+Observação: Arquivo será responsável por armazenar diferentes variáveis e principalmente a configuração da nossa aplicação.
+Author: Glaucia Lemos
+
+*/
+
+module.exports = {
+    'configName': 'node-auth',
+    'database' : 'mongodb://glemos:glau123@ds060649.mlab.com:60649/node-auth' // via mLab
+    //'database': 'mongodb://localhost/node-auth' // localmente
+}
